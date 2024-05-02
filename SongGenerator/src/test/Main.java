4a13f4b34c1a48527e4e8c943ff87cbf8c92b57e
djhkbw <djhkbw@umsystem.edu>
2024-05-01 22:53:11 -0500

    testing, will remove later

--- SongGenerator/src/test/Main.java
+++ SongGenerator/src/test/Main.java
@@ -4,7 +4,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 Song one = new Song("One", Genre.ROCK, "Joker", 1.28);
+		 Song two = new Song("Two", Genre.POP, "Batman", 2.10);
+		 Song three = new Song("Three", Genre.CLASSICAL, "Quinn", 3.50);
+		 
+		 Playlist jams = new Playlist();
+		 jams.addSong(one);
+		 jams.addSong(two);
+		 jams.addSong(three);
+		 
+		 jams.printSongs();
+		 System.out.println(jams.getTotalRuntime());
 	}
 
 }
